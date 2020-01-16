f640d9e69fe2cdd8585195fb497882ea34ea519f
Vinicius Costa <vinni.jr@gmail.com>
2020-01-16 14:51:53 -0300

    Alterações

--- gcApp/src/pages/Index.vue
+++ gcApp/src/pages/Index.vue
@@ -1,11 +1,36 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
-  </q-page>
+  <q-page class="flex flex-center"> AQUI:: {{ this.contatos }} </q-page>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: "PageIndex"
+  name: "PageIndex",
+  created: function() {
+    this.loadData();
+  },
+  data() {
+    return {
+      contatos: [],
+      contato: {
+        nome: null,
+        valor: null,
+        obs: null,
+        canal: null
+      },
+      leftDrawerOpen: false
+    };
+  },
+  methods: {
+    loadData() {
+      debugger;
+      axios
+        .get("localhost:8888/gestao_contato/listar")
+        .then(response => {
+          this.contatos = response.data;
+        })
+        .catch(() => {});
+    }
+  }
 };
 </script>
