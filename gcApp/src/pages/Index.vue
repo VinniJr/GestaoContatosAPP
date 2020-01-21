22c36b7011e1f489e417fd18aef3d4aa2b1fc7dc
Vinicius Costa <vinni.jr@gmail.com>
2020-01-21 15:06:54 -0300

    Implementando CRUD

--- gcApp/src/pages/Index.vue
+++ gcApp/src/pages/Index.vue
@@ -7,37 +7,41 @@
         <q-separator />
         <br />
         <br />
-        <q-input
-          v-model="contato.nome"
-          label="Nome"
-          :rules="[val => !!val || 'Campo Obrigatório']"
-        />
-        <q-input
-          v-model="contato.valor"
-          label="Valor"
-          :rules="[val => !!val || 'Campo Obrigatório']"
-        />
-        <q-input
-          v-model="contato.obs"
-          label="Observacao"
-          :rules="[val => !!val || 'Campo Obrigatório']"
-        />
+        <q-input v-model="contato.nome" label="Nome" />
+        <q-input v-model="contato.valor" label="Valor" />
+        <q-input v-model="contato.obs" label="Observacao" />
         <q-select v-model="contato.canal" :options="canais" label="Canal" />
       </div>
       <br />
       <q-btn color="primary" label="Salvar" @click="salvar" />
+      <q-btn
+        color="secondary"
+        label="Atualizar"
+        v-show="selected.length == 1"
+        @click="atualizar"
+      />
+      <q-btn
+        color="deep-orange"
+        glossy
+        label="Excluir"
+        v-show="selected.length == 1"
+        @click="excluir"
+      />
       <br />
       <br />
-      <div v-if="contatos.length !== 0">
-        <template>
+
+      <template>
+        <div v-if="contatos.length !== 0">
           <q-table
             title="Contatos"
             :data="contatos"
             :columns="columns"
-            row-key="name"
+            selection="single"
+            :selected.sync="selected"
+            row-key="nome"
           />
-        </template>
-      </div>
+        </div>
+      </template>
     </q-card-section>
   </q-page>
 </template>
@@ -49,8 +53,12 @@ export default {
   created: function() {
     this.loadData();
   },
+  deleteRow() {
+    alert("Veio");
+  },
   data() {
     return {
+      selected: [],
       contatos: [],
       contato: {
         nome: null,
@@ -107,30 +115,56 @@ export default {
   methods: {
     limpar() {
       this.contato = {
-        nome: "",
-        valor: "",
-        obs: "",
-        canal: ""
+        id: null,
+        nome: null,
+        valor: null,
+        obs: null,
+        canal: null
       };
+      this.selected = [];
     },
-    async salvar() {
+    atualizar() {
+      this.contato = this.selected[0];
+      console.log(this.contato);
+    },
+    async excluir() {
       await axios
-        .post("http://localhost:8888/gestao_contato/cadastrar", {
-          nome: this.contato.nome,
-          valor: this.contato.valor,
-          obs: this.contato.obs,
-          canal: this.contato.canal
+        .delete(
+          "http://localhost:8888/gestao_contato/excluir/" + this.selected[0].id
+        )
+        .then(response => {
+          console.log(response);
         })
-        .then(function(response) {
-          console.log(response + " - " + this.contato);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-      this.limpar();
+        .catch(() => {});
       this.loadData();
     },
+    async salvar() {
+      if (
+        this.contato.nome != null &&
+        this.contato.valor != null &&
+        this.contato.canal != null
+      ) {
+        await axios
+          .post("http://localhost:8888/gestao_contato/cadastrar", {
+            id: this.contato.id,
+            nome: this.contato.nome,
+            valor: this.contato.valor,
+            obs: this.contato.obs,
+            canal: this.contato.canal
+          })
+          .then(function(response) {
+            console.log(response + " - " + this.contato);
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+        this.loadData();
+      } else {
+        alert("Preencha todos os campos");
+      }
+    },
     async loadData() {
+      this.limpar();
       await axios
         .get("http://localhost:8888/gestao_contato/listar")
         .then(response => {
